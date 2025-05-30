e56e61e5f5631e72ba23042b3a19c25b4c10ef9c
sajidnawazchaudhry <Sajid.Chaudhry.extern@dvag.com>
2025-05-30 17:37:09 +0200

    [INFO-7976]: basic project structure with release.yml

---
+++ src/main/kotlin/com/dvag/api/releaseworkflow/ReleaseWorkflowApplication.kt
@@ -0,0 +1,11 @@
+package com.dvag.api.releaseworkflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReleaseWorkflowApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReleaseWorkflowApplication>(*args)
+}
